ad28944e372dca58f66d5dcee241bfa8c4646b9e
maksim-ka <me@maksim-ka@.xyz>
2021-01-20 04:17:45 +0100

    changed location

--- hosts/personal.nix
+++ hosts/personal.nix
@@ -4,7 +4,7 @@ with lib;
 {
   networking.hosts =
     let hostConfig = {
-          "192.168.1.2"  = [ "ao" ];
+          "192.168.1.2"  = [ "nixlos" ];
           "192.168.1.3"  = [ "kiiro" ];
           "192.168.1.10" = [ "kuro" ];
           "192.168.1.11" = [ "shiro" ];
@@ -15,12 +15,13 @@ with lib;
     in mkIf (builtins.elem hostName hosts) hostConfig;
 
   ## Location config -- since Toronto is my 127.0.0.1
-  time.timeZone = mkDefault "America/Toronto";
+  time.timeZone = mkDefault "Europe/Berlin";
   i18n.defaultLocale = mkDefault "en_US.UTF-8";
   # For redshift, mainly
-  location = (if config.time.timeZone == "America/Toronto" then {
-    latitude = 43.70011;
-    longitude = -79.4163;
+#TODO: set loctions for hq's - consider useing other variable than timeZone
+  location = (if config.time.timeZone == "Europe/Berlin" then {
+    latitude = 49.88240;
+    longitude = 8.67044;
   } else if config.time.timeZone == "Europe/Copenhagen" then {
     latitude = 55.88;
     longitude = 12.5;
